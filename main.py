6feb13ba61a321ce1ca0ce947675439d0b3411f9
Devashish Prasad <devashishkprasad@gmail.com>
2018-06-28 12:28:07 +0530

    Updated with comments

--- main.py
+++ main.py
@@ -1,25 +1,26 @@
 import cv2
 import numpy as np
-from matplotlib import pyplot as plt
 import imutils
 import pytesseract
 
+# read image from disk
 image = cv2.imread('test13.jpg')
+# make it gray
 img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# blur it to remove noise
 img = cv2.GaussianBlur(img, (7,7), 0)
 
 # perform edge detection, then perform a dilation + erosion to
 # close gaps in between object edges
 edged = cv2.Canny(img, 40, 90)
 dilate = cv2.dilate(edged, None, iterations=2)
+# perform erosion if necessay, it completely depends on the image
 # erode = cv2.erode(dilate, None, iterations=1)
 
-# ret2,th1 = cv2.threshold(dilate,0,255,cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
-
-# ?th1 = cv2.GaussianBlur(dilate, (7,7), 0)
-
+# create an empty masks
 mask = np.ones(img.shape[:2], dtype="uint8") * 255
 
+# find contours
 cnts = cv2.findContours(dilate.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 cnts = cnts[0] if imutils.is_cv2() else cnts[1]
 
@@ -30,28 +31,26 @@ for c in cnts:
         cv2.drawContours(mask, [c], -1, 0, -1)
     
     x,y,w,h = cv2.boundingRect(c)
-
+    
+    # filter more contours if nessesary
     if(w>h):
         cv2.drawContours(mask, [c], -1, 0, -1)
-    # draw the contours on the image
-
-    # cv2.drawContours(orig, [box.astype("int")], -1, (0, 255, 0), 2)
-    # cv2.rectangle(orig,(x,y),(x+w,y+h),(0,255,0),2)
-
+    
 newimage = cv2.bitwise_and(dilate.copy(), dilate.copy(), mask=mask)
 img2 = cv2.dilate(newimage, None, iterations=3)
-ret2,th1 = cv2.threshold(img2,0,255,cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
+ret2,th1 = cv2.threshold(img2 ,0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
 
+# Tesseract OCR on the image
 temp = pytesseract.image_to_string(th1)
-cv2.putText(image,temp,(100,100),cv2.FONT_HERSHEY_SIMPLEX,1.8,(0,255,255),3)
+# Write results on the image
+cv2.putText(image, temp, (100,100), cv2.FONT_HERSHEY_SIMPLEX, 1.8, (0,255,255), 3)
 
+# show the outputs
 cv2.imshow('Original image', cv2.resize(image,(640,480)))
 cv2.imshow('Dilated', cv2.resize(dilate,(640,480)))
 cv2.imshow('New Image', cv2.resize(newimage,(640,480)))
 cv2.imshow('Inverted Threshold', cv2.resize(th1,(640,480)))
-# cv2.imshow()
 
 cv2.waitKey(0)
-
 cv2.destroyAllWindows()
 
